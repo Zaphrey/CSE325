94e4786717edc2cc58eed6fdaadf43d3856b33a6
Zachary Humphreys <zachary.humphreys32@gmail.com>
2025-09-25 14:41:01 -0400

    Completed unit 1 work

---
+++ U1_ContosoPizza/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace U1_ContosoPizza;
+
+public class WeatherForecast
+{
+    public DateOnly Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
